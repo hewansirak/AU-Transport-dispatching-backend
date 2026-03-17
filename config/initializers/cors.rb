42307e01ccd48db7abc62fc732aba684cffd3d00
hewansirak <hewansg@gmail.com>
2026-03-17 12:26:08 +0300

    Configured application & initialized cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,13 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins ENV.fetch("FRONTEND_URL", "http://localhost:3000")
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ["Authorization"]
+  end
+end
